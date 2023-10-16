76c4b2962ec091ab9a8ec5b602966142c9c541fa
Adel Ansari <adelansari.a@gmailcom>
2023-10-16 13:50:09 +0300

    editing main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AudioManager } from "./components/AudioManager";
+import Transcript from "./components/Transcript";
+import { useTranscriber } from "./hooks/useTranscriber";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const transcriber = useTranscriber();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="container flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center">
+          AudioTrans ML
+        </h1>
+        <h2 className="mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl">
+          ML-powered speech recognition directly in your browser
+        </h2>
+        <AudioManager transcriber={transcriber} />
+        <Transcript transcribedData={transcriber.output} />
+      </div>
+
+      <div className="absolute bottom-4">
+        Github{" "}
+        <a
+          className="underline"
+          href="https://github.com/adelansari/audiotrans-ml"
+        >
+          Repository
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
